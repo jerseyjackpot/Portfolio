90868c298f687c05686c1d6d4a247d079f7468d5
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-17 14:33:11 -0400

    working on updated page, added css and js

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+  });
+
+// function sendEmail()){
+//     var submitButton;
+    
+// }
