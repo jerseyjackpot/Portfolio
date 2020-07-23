2fd0b41646dd6bed8cbf3d5280405164f62243ef
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-22 22:23:54 -0400

    updated style, added anime, fixed nav

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+  });
+
+// function sendEmail()){
+//     var submitButton;
+    
+// }
+
+
+// $("#body-for-email").on("change", function(event){
+// $("#email-for-class").attr("href", 'mailto:amaddatu@gmail.com?' + jQuery.param({body:   $('#body-for-email').val(), subject: "My first email"}));
+// });
